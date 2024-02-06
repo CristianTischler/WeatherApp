90241b07aa8a2df0eb7205ce50f6b0923860ccce
Cristian Tischler <tischlercristian@gmail.com>
2024-02-06 12:25:19 -0300

    feat: env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,6 @@
 export const config = {
-  apiUrl: "https://api.openweathermap.org/data/3.0/onecall",
-  apiKey: "62d13e81903d31704da30c68f04b1d06",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  // apiUrl: "https://api.openweathermap.org/data/3.0/onecall",
+  // apiKey: "62d13e81903d31704da30c68f04b1d06",
 };
